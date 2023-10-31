ad10367bbd6148a6793bc1a226061037f4f225d8
佐藤昭文 <akfm.sato@gmail.com>
2023-10-31 23:25:26 +0900

    login server actions function define

--- apps/web/.eslintrc.js
+++ apps/web/.eslintrc.js
@@ -1,3 +1,7 @@
 module.exports = {
   extends: ["custom/next"],
+  rules: {
+    // server actions are async
+    "@typescript-eslint/no-misused-promises": "off",
+  },
 };
--- packages/ui/src/components/login-form.tsx
+++ packages/ui/src/components/login-form.tsx
@@ -1,27 +1,21 @@
 import * as React from "react";
 
-type Props = {
-  onSubmit: (event: React.FormEvent<HTMLFormElement>) => void;
-};
-
-export function LoginForm({ onSubmit }: Props): React.JSX.Element {
+export function LoginForm(): React.JSX.Element {
   return (
-    <form onSubmit={onSubmit}>
-      <div className="flex flex-col gap-y-3">
-        <input
-          className="input input-bordered w-full max-w-xs"
-          name="id"
-          placeholder="Your id"
-          type="text"
-        />
-        <input
-          className="input input-bordered w-full max-w-xs"
-          name="id"
-          placeholder="Password"
-          type="password"
-        />
-        <button className="btn btn-primary">login</button>
-      </div>
-    </form>
+    <div className="flex flex-col gap-y-3">
+      <input
+        className="input input-bordered w-full max-w-xs"
+        name="id"
+        placeholder="Your id"
+        type="text"
+      />
+      <input
+        className="input input-bordered w-full max-w-xs"
+        name="id"
+        placeholder="Password"
+        type="password"
+      />
+      <button className="btn btn-primary">login</button>
+    </div>
   );
 }
